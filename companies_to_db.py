2eceacc5713e90af8b77a8ff18c029d9b3fcadc8
Alex Cho <alex.jhcho@gmail.com>
2024-02-11 13:57:47 -0800

    change csv to spy then add companies later

--- companies_to_db.py
+++ companies_to_db.py
@@ -9,12 +9,12 @@ from tradelens import app, db
 import csv
 
 
-def nasdaq_to_db(filename):
+def companies_to_db(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         reader = csv.reader(file) # create a csv reader object
         next(reader) # skip the header row
         for row in reader:
-            ticker_symbol, company_name = row[:2] # get the first two cols: ticker symbol and company name
+            ticker_symbol, company_name = row[1], row[3] # get the columns: ticker symbol and company name
             stock = Stocks(ticker_symbol=ticker_symbol, company_name=company_name) # create a Stocks object with the extracted data
             db.session.add(stock)
         db.session.commit()
@@ -22,4 +22,4 @@ def nasdaq_to_db(filename):
 
 if __name__ == '__main__':
     with app.app_context():
-        nasdaq_to_db('nasdaq.csv')
+        companies_to_db('companies.csv')
