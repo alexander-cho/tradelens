e143db8100e6a95b825e0e641e965828c045b237
Alex Cho <alex.jhcho@gmail.com>
2024-02-20 21:11:08 -0800

    consider swtiching back to table form for tutes data

--- info_to_inject.py
+++ info_to_inject.py
@@ -23,5 +23,17 @@ print(sofi.get_calendar())
 'Revenue Average': 568380000}
 '''
 
+hist_data = sofi.history(period="1d")
+
+# Get the first row which corresponds to the last trading day
+last_day = hist_data.iloc[0]
+
+print(last_day['Open'])
+print(last_day['High'])
+print(last_day['Low'])
+print(last_day['Close'])
+print(last_day['Volume'])
+
+
 print(sofi.get_fast_info()['lastPrice']) # this gets live volume??
 
