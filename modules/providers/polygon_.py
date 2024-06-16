83f8bebc42b6fe139c68ecbd4c810f14c69c7313
Alex Cho <alex.jhcho@gmail.com>
2024-06-15 20:28:20 -0700

    feat: get working plotly graph with price/volume rows from polygon agg data

--- modules/providers/polygon_.py
+++ modules/providers/polygon_.py
@@ -1,30 +1,117 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import pytz
+
+import plotly.graph_objects as go
+import plotly.subplots as subplots
+
 from polygon import RESTClient
 
-client = RESTClient(api_key="<API_KEY>")
 
-ticker = "SOFI"
+load_dotenv()
 
-# List Aggregates (Bars)
-aggs = []
-for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
-    aggs.append(a)
+api_key = os.getenv('POLYGON_API_KEY')
 
-print(aggs)
+client = RESTClient(api_key)
 
-# Get Last Trade
-trade = client.get_last_trade(ticker=ticker)
-print(trade)
 
-# List Trades
-trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
-for trade in trades:
-    print(trade)
+ticker = 'SOFI'
+multiplier = 1
+timespan = 'hour'
+from_ = '2024-06-10'
+to = '2024-06-14'
+limit = 50000
 
-# Get Last Quote
-quote = client.get_last_quote(ticker=ticker)
-print(quote)
 
-# List Quotes
-quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
-for quote in quotes:
-    print(quote)
+bars = []
+
+for a in client.list_aggs(
+    ticker=ticker,
+    multiplier=multiplier,
+    timespan=timespan,
+    from_=from_,
+    to=to,
+    limit=limit
+):
+    bars.append(a)
+
+
+# Define timezones
+utc_zone = pytz.utc
+pst_zone = pytz.timezone('US/Pacific')
+
+
+# extract each attribute from aggregate bars
+timestamps = [bar.timestamp for bar in bars]
+opens = [bar.open for bar in bars]
+highs = [bar.high for bar in bars]
+lows = [bar.low for bar in bars]
+closes = [bar.close for bar in bars]
+volumes = [bar.volume for bar in bars]
+number_of_transactions = [bar.transactions for bar in bars]
+
+
+# Convert timestamps to datetime objects in UTC
+datetime_utc = [datetime.utcfromtimestamp(ts / 1000).replace(tzinfo=utc_zone) for ts in timestamps]
+
+# Convert UTC datetime to PST
+datetime_pst = [dt_utc.astimezone(pst_zone) for dt_utc in datetime_utc]
+
+# Extract datetime strings for plotting
+datetime_strings = [dt_pst.strftime('%Y-%m-%d %H:%M:%S') for dt_pst in datetime_pst]
+
+
+# make subplot of two rows to plot two graphs sharing an x-axis, the timestamps.
+fig = subplots.make_subplots(
+    rows=2,
+    cols=1,
+    shared_xaxes=True,
+    vertical_spacing=0.02,
+    row_heights=[0.7, 0.3]
+)
+
+# Add candlestick trace to row one of subplot
+fig.add_trace(
+    go.Candlestick(
+        x=datetime_strings,
+        open=opens,
+        high=highs,
+        low=lows,
+        close=closes,
+        name='Price'
+    ),
+    row=1,
+    col=1
+)
+
+# Add volume bar trace to row two of subplot
+fig.add_trace(
+    go.Bar(
+        x=datetime_strings,
+        y=volumes,
+        name='Volume',
+        yaxis='y2'
+    ),
+    row=2,
+    col=1
+)
+
+
+# Update the layout for better visualization
+fig.update_layout(
+    title=f'Chart for {ticker} ({multiplier} {timespan})',
+    yaxis1_title='Share price',
+    yaxis2_title='Volume (M)',
+    xaxis1_title='Time',
+    xaxis_rangeslider_visible=False
+)
+
+
+# get rid of weekend/holiday market closure gaps
+fig.layout.xaxis.type = 'category'
+
+
+fig.show()
+
+# print(bars)
