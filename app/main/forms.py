f751f4017f99aa1986561b2877316960edbb1910
Alex Cho <alex.jhcho@gmail.com>
2024-07-05 23:59:10 -0700

    feat: start UI for symbol front page

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 # create a search form
 class SearchForm(FlaskForm):
-    searched = StringField("Searched",
-                           validators=[DataRequired()])  # name="searched" attribute, in <form> of navbar.html
+    # name="searched" attribute, in <form> of navbar.html
+    searched = StringField("Searched", validators=[DataRequired()])
     submit = SubmitField("Submit")
