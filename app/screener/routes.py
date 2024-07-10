ce54f2c098b91e9770c2f331b49f43ac275adffd
Alex Cho <alex.jhcho@gmail.com>
2024-07-10 16:21:19 -0700

    feat: screener now outputs bearish/bullish signal for each ticker that meets each pattern criteria

--- app/screener/routes.py
+++ app/screener/routes.py
@@ -1,10 +1,13 @@
-from flask import render_template, redirect, url_for, flash
+import talib
 import yfinance as yf
+
+from flask import render_template, request
 
 from app import db
 
 from ..models import Stocks
-from ..main.forms import SearchForm
+
+from modules.utils.date_ranges import get_date_range_past
 
 from . import bp_screener
 
@@ -13,36 +16,42 @@ from .patterns import candlestick_patterns
 
 @bp_screener.route('/technical-screener')
 def technical_screener():
-    stock_list = Stocks.query.all()
+    patterns = candlestick_patterns
+    stocks = db.session.query(Stocks).filter(Stocks.id < 101)
+    (start_date, today) = get_date_range_past(365)
+
+    # form select in template, name attribute is pattern, get the current selected pattern using this
+    current_pattern = request.args.get('pattern', None)
+    # pattern names are named attributes in ta-lib implementation, so pass the name of the function which is a string
+    # and pass is as a variable
+    pattern_data = {}
+    if current_pattern:
+        for stock in stocks:
+            pattern_function = getattr(talib, current_pattern)
+            try:
+                ohlcv_df = yf.download(tickers=stock.ticker_symbol, start=start_date, end=today)
+                result = pattern_function(ohlcv_df['Open'], ohlcv_df['High'], ohlcv_df['Low'], ohlcv_df['Close'])
+                # return just the last candlestick
+                last = result.tail(1).values[0]
+                if last > 0:
+                    pattern_data[stock.ticker_symbol] = 'bullish'
+                elif last < 0:
+                    pattern_data[stock.ticker_symbol] = 'bearish'
+                else:
+                    pass
+            except ValueError:
+                pass
+
+    screen = {
+        'pattern': current_pattern,
+        'data': pattern_data
+    }
+
     return render_template(
         template_name_or_list='screener/technical_screener.html',
         title='Technical Screener',
-        stock_list=stock_list
-    )
-
-
-@bp_screener.route('/screener-symbol-search', methods=["POST"])
-def screener_symbol_search():
-    symbol_search_form = SearchForm()
-    if symbol_search_form.validate_on_submit():
-        search_content = symbol_search_form.searched.data.upper()
-        searched_ticker_found = Stocks.query.filter_by(ticker_symbol=search_content).first()
-        if searched_ticker_found:
-            return redirect(url_for('screener.symbol_screen', symbol=search_content))
-        else:
-            flash("That stock does not exist or is not in our database yet")
-            return redirect(url_for('screener.screener_symbol_search'))
-    else:
-        return redirect(url_for('screener.screener_symbol_search'))
-
-
-@bp_screener.route('/technical-screener/<symbol>')
-def symbol_screen(symbol):
-    stock = db.session.query(Stocks).filter(Stocks.ticker_symbol == symbol).first()
-    patterns = candlestick_patterns
-    return render_template(
-        template_name_or_list='screener/symbol_screen.html',
-        title=f'Screen {stock.ticker_symbol}',
-        stock=stock,
-        patterns=patterns
+        stocks=stocks,
+        patterns=patterns,
+        current_pattern=current_pattern,
+        screen=screen
     )
