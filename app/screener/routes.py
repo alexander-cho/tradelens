c47896818147a8da05e69e444cb74df37c5b00da
Alex Cho <alex.jhcho@gmail.com>
2024-07-09 23:54:52 -0700

    feat: set up technical screener functionality

--- app/screener/routes.py
+++ app/screener/routes.py
@@ -1,17 +1,48 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+import yfinance as yf
 
-from modules.providers.polygon_ import Polygon
+from app import db
+
+from ..models import Stocks
+from ..main.forms import SearchForm
 
 from . import bp_screener
 
-from .patterns import sofi
+from .patterns import candlestick_patterns
 
 
 @bp_screener.route('/technical-screener')
 def technical_screener():
-    sofi_df = sofi
+    stock_list = Stocks.query.all()
     return render_template(
         template_name_or_list='screener/technical_screener.html',
         title='Technical Screener',
-        sofi_df=sofi_df
+        stock_list=stock_list
+    )
+
+
+@bp_screener.route('/screener-symbol-search', methods=["POST"])
+def screener_symbol_search():
+    symbol_search_form = SearchForm()
+    if symbol_search_form.validate_on_submit():
+        search_content = symbol_search_form.searched.data.upper()
+        searched_ticker_found = Stocks.query.filter_by(ticker_symbol=search_content).first()
+        if searched_ticker_found:
+            return redirect(url_for('screener.symbol_screen', symbol=search_content))
+        else:
+            flash("That stock does not exist or is not in our database yet")
+            return redirect(url_for('screener.screener_symbol_search'))
+    else:
+        return redirect(url_for('screener.screener_symbol_search'))
+
+
+@bp_screener.route('/technical-screener/<symbol>')
+def symbol_screen(symbol):
+    stock = db.session.query(Stocks).filter(Stocks.ticker_symbol == symbol).first()
+    patterns = candlestick_patterns
+    return render_template(
+        template_name_or_list='screener/symbol_screen.html',
+        title=f'Screen {stock.ticker_symbol}',
+        stock=stock,
+        patterns=patterns
     )
