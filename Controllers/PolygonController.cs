a576594811473bbd65a8c6536c3c461ec4b7d089
Alex Cho <alex.jhcho@gmail.com>
2024-09-26 22:38:19 -0700

    feat: modify polygon endpoints, add tradier endpoint for options chain

--- Controllers/PolygonController.cs
+++ Controllers/PolygonController.cs
@@ -13,11 +13,11 @@ namespace TradeLens.Controllers
     public class PolygonController: ControllerBase
     {
         public static readonly HttpClient _httpClient;
-        public string stocksTicker = "SOFI";
+        public string ticker = "SOFI";
         public int multiplier = 1;
         public string timespan = "day";
-        public string startDate = "2024-09-01";
-        public string endDate = "2024-09-24";
+        public string startDate = "2024-09-02";
+        public string endDate = "2024-09-25";
 
         static PolygonController()
         {
@@ -25,37 +25,30 @@ namespace TradeLens.Controllers
             _httpClient = new HttpClient();
         }
 
-        [HttpGet("Ticker")]
-        public async Task GetTickerAggs(HttpClient _httpClient)
+        // GET: api/v1/Polygon/BarAggs
+        [HttpGet("BarAggs")]
+        public async Task<ActionResult> GetBarAggs(HttpClient _httpClient)
         {
             var POLYGON_API_KEY = Environment.GetEnvironmentVariable("POLYGON_API_KEY");
-            using HttpResponseMessage response = await _httpClient.GetAsync($"https://api.polygon.io/v2/aggs/ticker/{this.stocksTicker}/range/{this.multiplier}/{this.timespan}/{this.startDate}/{this.endDate}?adjusted=true&sort=asc&apiKey={POLYGON_API_KEY}");
+            Uri BaseAddress = new Uri($"https://api.polygon.io/v2/aggs/ticker/{this.ticker}/range/{this.multiplier}/{this.timespan}/{this.startDate}/{this.endDate}?adjusted=true&sort=asc&apiKey={POLYGON_API_KEY}");
+            using HttpResponseMessage response = await _httpClient.GetAsync(BaseAddress);
             response.EnsureSuccessStatusCode();
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"{jsonResponse}\n");
-            return;
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var jsonResponse = Content(stringResponse, "application/json");
+            return jsonResponse;
         }
 
-        [HttpGet("Options")]
-        public async Task GetOptionAggs(HttpClient _httpClient)
+        // GET: api/v1/Polygon/RelatedCompanies
+        [HttpGet("RelatedCompanies")]
+        public async Task<ActionResult> GetRelatedCompanies(HttpClient _httpClient)
         {
             var POLYGON_API_KEY = Environment.GetEnvironmentVariable("POLYGON_API_KEY");
-            using HttpResponseMessage response = await _httpClient.GetAsync($"https://api.polygon.io/v2/aggs/ticker/O:SOFI241004C00008000/range/1/day/2024-09-12/2024-09-24?adjusted=true&sort=asc&apiKey={POLYGON_API_KEY}");
+            Uri BaseAddress = new Uri($"https://api.polygon.io/v1/related-companies/{this.ticker}?apiKey={POLYGON_API_KEY}");
+            using HttpResponseMessage response = await _httpClient.GetAsync(BaseAddress);
             response.EnsureSuccessStatusCode();
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"{jsonResponse}\n");
-            return;
-        }
-
-        [HttpGet("Crypto")]
-        public async Task GetCryptoAggs(HttpClient _httpClient)
-        {
-            var POLYGON_API_KEY = Environment.GetEnvironmentVariable("POLYGON_API_KEY");
-            using HttpResponseMessage response = await _httpClient.GetAsync($"https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/2024-01-09/2024-09-09?apiKey={POLYGON_API_KEY}");
-            response.EnsureSuccessStatusCode();
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"{jsonResponse}\n");
-            return;
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var jsonResponse = Content(stringResponse, "application/json");
+            return jsonResponse;
         }
     }
 }
