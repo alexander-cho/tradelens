11d8e989a360082860013ac19a07e574bfc9feaf
Alex Cho <alex.jhcho@gmail.com>
2024-10-06 17:07:48 -0700

    feat(cli): start sqlite dbcontext

---
+++ cli/src/TradeLensCLI/Models/Post.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TradeLensCLI.Models
+{
+    public class Post
+    {
+        public int Id { get; }
+        [Required]
+        public string? Ticker { get; set; }
+        [Required]
+        public string? Body { get; set; }
+        [Required]
+        public string? Sentiment { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
--- cli/src/TradeLensCLI/Program.cs
+++ cli/src/TradeLensCLI/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace TradeLensCLI;
+﻿namespace TradeLensCli;
 
 class Program
 {
