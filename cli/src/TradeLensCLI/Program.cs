b835d6e515a791a16daac72641003e428f9b1382
Alex Cho <alex.jhcho@gmail.com>
2024-10-21 23:23:43 -0700

    feat(cli): misc controller commands

--- cli/src/TradeLensCLI/Program.cs
+++ cli/src/TradeLensCLI/Program.cs
@@ -13,9 +13,18 @@ namespace TradeLensCLI
             using (CliDbContext dbContext = new CliDbContext())
             {
                 Console.WriteLine("Hello");
+                // break down using helpers if needed
+                // while the user doesn't type 'q' to exit the program
+
+                // ask to:
+                // add a post, view the whole list, get the posts associated with a specific ticker
+                // switch statements
+                // if user grabs ticker posts, display and choose one of them?
+                // 
+                // do validation to see if it is a valid ticker
                 PostController postController = new PostController(dbContext);
-                // postController.GetAllPosts();
-                postController.AddPost();
+                postController.GetAllPosts();
+                // postController.AddPost();
             }
         }
     }
