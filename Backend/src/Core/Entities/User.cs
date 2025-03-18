95567c924ae9cea226196ba1b193c1b56663f420
Alex Cho <alex.jhcho@gmail.com>
2025-03-17 20:04:20 -0700

    refactor: lowercase backend dir name

--- Backend/src/Core/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Core.Entities;
-
-public class User : IdentityUser
-{
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-}
