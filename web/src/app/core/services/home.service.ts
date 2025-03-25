f39b6b547fedf9a5ff0a69a866be1e45b44d0a94
Alex Cho <alex.jhcho@gmail.com>
2025-03-24 19:03:55 -0700

    feat(web): start client-side homepage market status

---
+++ web/src/app/core/services/home.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { MarketStatus } from '../../shared/models/finnhub';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  private baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  public getMarketStatus() {
+    return this.http.get<MarketStatus>(this.baseUrl + 'finnhub/marketstatus');
+  }
+}
