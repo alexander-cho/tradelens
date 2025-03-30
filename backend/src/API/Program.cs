b093c0059c97a524767decb02b62ee94a6056409
Alex Cho <alex.jhcho@gmail.com>
2025-03-30 00:03:05 -0700

    refactor: add extension method database migrations and seeding

--- backend/src/API/Program.cs
+++ backend/src/API/Program.cs
@@ -1,11 +1,6 @@
 using API.Extensions;
 using API.Middleware;
 using Core.Entities;
-using Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
-using Npgsql;
-using Polly;
-using Polly.Retry;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -49,38 +44,8 @@ app.MapGroup("api").MapIdentityApi<User>();
 // if API server cannot handle request, gets passed to client app
 app.MapFallbackToController("Index", "Fallback");
 
-// re-seeding the db with data when restarting the application server
-try
-{
-    using var scope = app.Services.CreateScope();
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<TradelensDbContext>();
-    
-    // retry policy for Npgsql exceptions
-    AsyncRetryPolicy retryPolicy = Policy
-        .Handle<NpgsqlException>()
-        .WaitAndRetryAsync(
-            retryCount: 5,
-            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
-            onRetry: (exception, retryCount, context) =>
-            {
-                Console.WriteLine($"Retry {retryCount} due to {exception.Message}");
-            });
-
-    await retryPolicy.ExecuteAsync(async () =>
-    {
-        await context.Database.MigrateAsync();
-    });
-    
-    await DbContextSeed.SeedPostsAsync(context);
-    await DbContextSeed.SeedCompanyData(context);
-}
-catch (Exception exception)
-{
-    Console.WriteLine(exception);
-    throw;
-}
+await app.MigrateAndSeedDatabaseAsync();
 
 app.Run();
 
-public static partial class Program {}
+public partial class Program {}
