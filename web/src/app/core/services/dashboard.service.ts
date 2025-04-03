dfd6df2eaa886d2739f742ef099115fe5577798f
Alex Cho <alex.jhcho@gmail.com>
2025-04-03 13:42:09 -0700

    feat(web): add candlestick component charting utility for dashboard

--- web/src/app/core/services/dashboard.service.ts
+++ web/src/app/core/services/dashboard.service.ts
@@ -2,6 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { BarAggregates, RelatedCompanies } from '../../shared/models/polygon';
+import { Observable } from 'rxjs';
+import { Stock } from '../../shared/models/stock';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class DashboardService {
   private baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
 
-  public getBarAggregates(ticker: string, multiplier: number, timespan: string, from: string, to: string) {
+  public getBarAggregates(ticker: string, multiplier: number, timespan: string, from: string, to: string): Observable<BarAggregates> {
     let params = new HttpParams();
     if (ticker) {
       params = params.append('ticker', ticker);
@@ -30,11 +32,16 @@ export class DashboardService {
     return this.http.get<BarAggregates>(this.baseUrl + 'polygon/baraggs', { params });
   }
 
-  public getRelatedCompanies(ticker: string) {
+  public getRelatedCompanies(ticker: string): Observable<RelatedCompanies> {
     let params = new HttpParams();
     if (ticker) {
       params = params.append('ticker', ticker);
     }
     return this.http.get<RelatedCompanies>(this.baseUrl + 'polygon/relatedcompanies', { params });
+  }
+
+  // method in order to associate ticker retrieved from parent component with stock entity
+  public getStockByTicker(ticker: string): Observable<Stock> {
+    return this.http.get<Stock>(this.baseUrl + 'stocks/' + ticker);
   }
 }
