4337ddb794ef2aa19abdf24ec3a53def60168790
Alex Cho <alex.jhcho@gmail.com>
2025-09-22 23:10:40 -0700

    fix: change db seed config for dev environment

--- backend/src/Infrastructure/Data/DbContextSeed.cs
+++ backend/src/Infrastructure/Data/DbContextSeed.cs
@@ -9,19 +9,22 @@ public static class DbContextSeed
     public static async Task SeedPostsAsync(TradelensDbContext context)
     {
         // var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        var path = Path.Combine("Infrastructure", "Data", "SeedData", "posts.json");
         
-        // read data from products.json seed file if DB is empty
+        // read data from posts.json seed file if DB is empty
         if (!context.Posts.Any())
         {
             // in production
             // var postsData = await File.ReadAllTextAsync(path + @"/Data/SeedData/posts.json");
             
-            // var postsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/posts.json");
+            // in development
+            var postsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/posts.json");
             
-            var postsData = await File.ReadAllTextAsync(path);
+            // for docker-compose (?)
+            // var path = Path.Combine("Infrastructure", "Data", "SeedData", "posts.json");
+            // var postsData = await File.ReadAllTextAsync(path);
             
             var posts = JsonSerializer.Deserialize<List<Post>>(postsData);
+            
             // if there is no data
             if (posts == null)
             {
@@ -36,18 +39,18 @@ public static class DbContextSeed
     public static async Task SeedCompanyData(TradelensDbContext context)
     {
         // var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        var path = Path.Combine("Infrastructure", "Data", "SeedData", "tickers.json");
         
         if (!context.Stocks.Any())
         {
             // var stocksData = await File.ReadAllTextAsync(path + @"/Data/SeedData/tickers.json");
             
-            // var stocksData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/tickers.json");
+            var stocksData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/tickers.json");
             
-            var stocksData = await File.ReadAllTextAsync(path);
+            // var path = Path.Combine("Infrastructure", "Data", "SeedData", "tickers.json");
+            // var stocksData = await File.ReadAllTextAsync(path);
+            
             var stocks = JsonSerializer.Deserialize<List<Stock>>(stocksData);
     
-            // if there is no data
             if (stocks == null)
             {
                 return;
