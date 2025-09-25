1457ec5e70caedef93b24d49cb143aa6a64bd159
Alex Cho <alex.jhcho@gmail.com>
2025-09-24 23:37:30 -0700

    feat(web): implement max pain chart- functionality is there, but need to reset cash values when user enters another ticker, or selects a different expiration date

--- web/src/app/features/options/options.component.ts
+++ web/src/app/features/options/options.component.ts
@@ -1,36 +1,41 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { OptionsService } from '../../core/services/options.service';
 import { NavbarComponent } from '../../layout/navbar/navbar.component';
-import { MatButton } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { CallsAndPutsCashSums, ExpiryData } from '../../shared/models/options';
+import { MaxPainChartComponent } from './max-pain-chart/max-pain-chart.component';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzInputDirective } from 'ng-zorro-antd/input';
+import { NzOptionComponent, NzSelectComponent } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-options',
   imports: [
     NavbarComponent,
-    MatButton,
-    FormsModule
+    FormsModule,
+    MaxPainChartComponent,
+    NzButtonComponent,
+    NzInputDirective,
+    NzSelectComponent,
+    NzOptionComponent
   ],
   templateUrl: './options.component.html',
   styleUrl: './options.component.scss'
 })
-export class OptionsComponent implements OnInit {
+export class OptionsComponent {
   optionsService = inject(OptionsService);
   ticker = "";
   expirationDates?: ExpiryData;
+  selectedExpiration: string = "";
   maxPainData?: CallsAndPutsCashSums;
-
-  ngOnInit() {
-    // this.getExpirationDatesAssociatedWithStock();
-  }
 
   onTickerSubmit() {
     this.getExpirationDatesAssociatedWithStock();
-    this.getCashValuesAndMaxPAin();
   }
 
-  // create cash values chart
+  onExpiryDateSelection() {
+    this.getCashValuesAndMaxPain();
+  }
 
   getExpirationDatesAssociatedWithStock() {
     this.optionsService.getExpirations(this.ticker).subscribe({
@@ -42,10 +47,11 @@ export class OptionsComponent implements OnInit {
     });
   }
 
-  getCashValuesAndMaxPAin() {
-    this.optionsService.getCashValuesAndMaxPain(this.ticker, "2025-09-26").subscribe({
+  getCashValuesAndMaxPain() {
+    this.optionsService.getCashValuesAndMaxPain(this.ticker, this.selectedExpiration).subscribe({
       next: response => {
         this.maxPainData = response;
+        console.log(this.selectedExpiration);
         console.log(response);
       },
       error: err => console.log(err)
