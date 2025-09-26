3ad3efecd6decb3a819af9bf924dd917e9976b6c
Alex Cho <alex.jhcho@gmail.com>
2025-09-26 00:01:14 -0700

    feat: functionality for re-rendering charts for change in expiration date selection works now + reset on ticker symbol change- but need to update chart instead of destroy and re-create on change

--- web/src/app/features/options/max-pain-chart/max-pain-chart.component.ts
+++ web/src/app/features/options/max-pain-chart/max-pain-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, InputSignal, OnInit } from '@angular/core';
+import { Component, effect, input, InputSignal } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import { CallsAndPutsCashSums } from '../../../shared/models/options';
 
@@ -8,56 +8,60 @@ import { CallsAndPutsCashSums } from '../../../shared/models/options';
   templateUrl: './max-pain-chart.component.html',
   styleUrl: './max-pain-chart.component.scss'
 })
-export class MaxPainChartComponent implements OnInit {
+export class MaxPainChartComponent {
+  tickerSymbol: InputSignal<string | undefined> = input<string>();
   cashValuesDataForChart: InputSignal<CallsAndPutsCashSums | undefined> = input<CallsAndPutsCashSums>();
   expirationDate: InputSignal<string | undefined> = input<string>();
 
-  chart: any = [];
+  chart?: Chart;
 
-  ngOnInit() {
-    this.createMaxPainChart();
-  }
+  chartChangeEffect = effect(() => {
+    // read both signals to track changes
+    const newCashData = this.cashValuesDataForChart();
+    const newExpiryDate = this.expirationDate();
 
-  createMaxPainChart() {
-    if (this.cashValuesDataForChart) {
-      console.log(this.cashValuesDataForChart());
-      this.chart = new Chart('maxPainChart', {
-        type: 'bar',
-        data: {
-          labels: this.cashValuesDataForChart()?.callCashSums.map(x => x.price),
-          datasets: [
-            {
-              label: 'Calls',
-              data: this.cashValuesDataForChart()?.callCashSums.map(x => x.totalCashValue),
-              borderWidth: 1,
-              backgroundColor: 'rgba(0, 250, 0, 0.7)'
-            },
-            {
-              label: 'Puts',
-              data: this.cashValuesDataForChart()?.putCashSums.map(x => x.totalCashValue),
-              borderWidth: 1,
-              backgroundColor: 'rgba(250, 0, 0, 0.7)'
-            }
-          ],
-        },
-        options: {
-          scales: {
-            x: {
-              stacked: true
-            },
-            y: {
-              beginAtZero: true,
-              stacked: true
-            },
-          },
-          plugins: {
-            title: {
-              text: 'Cash Values for <ticker> expiring <date>',
-              display: true
-            }
-          }
-        },
-      });
+    if (!newCashData || !newCashData.callCashSums || !newCashData.putCashSums) {
+      return;
     }
-  }
+
+    this.chart?.destroy();
+
+    this.chart = new Chart('maxPainChart', {
+      type: 'bar',
+      data: {
+        labels: newCashData?.callCashSums.map(x => x.price),
+        datasets: [
+          {
+            label: 'Calls',
+            data: newCashData?.callCashSums.map(x => x.totalCashValue),
+            borderWidth: 1,
+            backgroundColor: 'rgba(0, 250, 0, 0.7)'
+          },
+          {
+            label: 'Puts',
+            data: newCashData?.putCashSums.map(x => x.totalCashValue),
+            borderWidth: 1,
+            backgroundColor: 'rgba(250, 0, 0, 0.7)'
+          }
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            stacked: true
+          },
+          y: {
+            beginAtZero: true,
+            stacked: true
+          },
+        },
+        plugins: {
+          title: {
+            text: `Cash Values for ${ this.tickerSymbol()?.toUpperCase() } expiring ${ newExpiryDate }`,
+            display: true
+          }
+        }
+      },
+    });
+  });
 }
