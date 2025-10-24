1554c3809d333b3d0df8be2bac4180df28e84a0f
Alex Cho <alex.jhcho@gmail.com>
2025-10-24 14:16:05 -0700

    feat: chart engine

--- client-ng/src/app/features/chart-engine/chart-engine.component.ts
+++ client-ng/src/app/features/chart-engine/chart-engine.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzInputDirective } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-chart-engine',
-  imports: [],
+  imports: [
+    FormsModule,
+    NzButtonComponent,
+    NzInputDirective,
+    ReactiveFormsModule
+  ],
   templateUrl: './chart-engine.component.html',
   styleUrl: './chart-engine.component.scss'
 })
 export class ChartEngineComponent {
+  ticker: WritableSignal<string | undefined> = signal(undefined);
 
+  availableMetrics: string[] = ['revenue', 'netIncome', 'grossProfit',
+    'totalAssets', 'totalLiabilities', 'totalStockholdersEquity',
+    'freeCashFlow', 'stockBasedCompensation', 'cashAtEndOfPeriod'];
+
+  onTickerSubmit() {
+    this.ticker.set(this.ticker()?.toUpperCase());
+    console.log(this.ticker());
+  }
 }
