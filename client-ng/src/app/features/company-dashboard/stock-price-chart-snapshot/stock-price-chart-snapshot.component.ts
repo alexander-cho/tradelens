7455e0297f1fea16196dfad37ea8417c1a4b6272
Alex Cho <alex.jhcho@gmail.com>
2025-11-25 23:13:20 -0800

    feat: remove hardcoded stock price from dates and use utility functions

--- client-ng/src/app/features/company-dashboard/stock-price-chart-snapshot/stock-price-chart-snapshot.component.ts
+++ client-ng/src/app/features/company-dashboard/stock-price-chart-snapshot/stock-price-chart-snapshot.component.ts
@@ -23,37 +23,54 @@ export class StockPriceChartSnapshotComponent implements OnInit {
   ticker: InputSignal<string> = input.required<string>();
 
   multiplier = 1;
-  timespan: WritableSignal<string> = signal('day');
+  timespan: WritableSignal<string> = signal('hour');
   from: WritableSignal<string> = signal('');
   to = this.getTodayDate();
   companyDashboardService = inject(CompanyDashboardService);
 
   barAggregates: WritableSignal<BarAggregates | undefined> = signal(undefined);
-  selectedChartOption: WritableSignal<string> = signal('1m');
+  selectedChartOption: WritableSignal<string> = signal('1d');
 
   setChartOptionsEffect = effect(() => {
-    if (this.selectedChartOption() == '1m') {
-      this.from.set('2025-10-25');
+    if (this.selectedChartOption() == '1d') {
+      this.from.set(this.getDaysAgo(1));
+      this.timespan.set('hour');
+      this.getBars();
+    } else if (this.selectedChartOption() == '1w') {
+      this.from.set(this.getWeeksAgo(1));
+      this.timespan.set('day');
+      this.getBars();
+    } else if (this.selectedChartOption() == '1m') {
+      this.from.set(this.getMonthsAgo(1));
       this.timespan.set('day');
       this.getBars();
     } else if (this.selectedChartOption() == '3m') {
-      this.from.set('2025-08-25');
+      this.from.set(this.getMonthsAgo(3));
+      this.timespan.set('day');
+      this.getBars();
+    } else if (this.selectedChartOption() == '6m') {
+      this.from.set(this.getMonthsAgo(6));
       this.timespan.set('day');
       this.getBars();
     } else if (this.selectedChartOption() == 'ytd') {
-      this.from.set('2025-01-01');
-      this.timespan.set('day');
+      this.from.set(this.getStartOfYear());
+      let toDate = new Date(this.to);
+      if (toDate.getMonth() > 6) {
+        this.timespan.set('week');
+      } else {
+        this.timespan.set('day');
+      }
       this.getBars();
     } else if (this.selectedChartOption() == '1y') {
-      this.from.set('2024-11-25');
+      this.from.set(this.getYearsAgo(1));
       this.timespan.set('week');
       this.getBars();
-    } else if (this.selectedChartOption() == '5y') {
-      this.from.set('2020-11-25');
-      this.timespan.set('week');
+    } else if (this.selectedChartOption() == '2y') {
+      this.from.set(this.getYearsAgo(2));
+      this.timespan.set('month');
       this.getBars();
     }
-  })
+  });
 
   stock?: Stock;
   chart?: Chart;
@@ -79,16 +96,54 @@ export class StockPriceChartSnapshotComponent implements OnInit {
   }
 
   // have this in a separate utils maybe
-  getTodayDate() {
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1;
-    let yyyy = today.getFullYear();
+  // for months just subtract x amount of months from MM portion of date string
+  // same for YYYY
+  getTodayDate(): string {
+    const today = new Date();
+    return this.formatDate(today);
+  }
 
-    let ddStr = dd < 10 ? '0' + dd : dd.toString();
-    let mmStr = mm < 10 ? '0' + mm : mm.toString();
+  getDaysAgo(days: number): string{
+    const date = new Date();
+    date.setDate(date.getDate() - days);
+    return this.formatDate(date);
+  }
 
-    return yyyy + '-' + mmStr + '-' + ddStr;
+  getWeeksAgo(weeks: number): string {
+    const date = new Date();
+    const daysInAWeek = 7
+    date.setDate(date.getDate() - weeks * daysInAWeek);
+    return this.formatDate(date);
+  }
+
+  getMonthsAgo(months: number): string {
+    const date = new Date();
+    date.setMonth(date.getMonth() - months);
+    return this.formatDate(date);
+  }
+
+  getYearsAgo(years: number): string {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() - years);
+    return this.formatDate(date);
+  }
+
+  getStartOfYear(): string {
+    const date = new Date();
+    date.setMonth(0); // January
+    date.setDate(1);
+    return this.formatDate(date);
+  }
+
+  formatDate(date: Date): string {
+    const dd = date.getDate();
+    const mm = date.getMonth() + 1;
+    const yyyy = date.getFullYear();
+
+    const ddStr = dd < 10 ? '0' + dd : dd.toString();
+    const mmStr = mm < 10 ? '0' + mm : mm.toString();
+
+    return `${yyyy}-${mmStr}-${ddStr}`;
   }
 
   isPriceUp(): boolean {
@@ -115,10 +170,14 @@ export class StockPriceChartSnapshotComponent implements OnInit {
       const date = new Date(x.t);
       const option = this.selectedChartOption();
 
-      if (option === '1m' || option === '3m') {
+      if (option === '1d' || option === '1w') {
+        return date.toLocaleDateString('en-US', { day: 'numeric', hour: '2-digit' });
+      } else if (option === '1m' || option === '3m' || option === '6m') {
         return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
-      } else if (option === 'ytd' || option === '1y' || option === '5y') {
+      } else if (option === 'ytd' || option === '1y') {
         return date.toLocaleDateString('en-US', { month: 'short' });
+      } else if (option === '2y') {
+        return date.toLocaleDateString('en-US', { year: 'numeric' , month: 'short'});
       }
       return '';
     });
@@ -164,7 +223,8 @@ export class StockPriceChartSnapshotComponent implements OnInit {
         ],
       },
       options: {
-        maintainAspectRatio: false,
+        maintainAspectRatio: true,
+        aspectRatio: 2.5,
         scales: {
           x: {
             ticks: {
