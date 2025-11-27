748b00cb1c8274eb24027332285e693db5a0af10
Alex Cho <alex.jhcho@gmail.com>
2025-11-27 12:38:27 -0800

    feat: change landing component logo

--- client-ng/src/app/features/auth/landing/landing.component.ts
+++ client-ng/src/app/features/auth/landing/landing.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
-import { NgOptimizedImage } from '@angular/common';
+import { NzIconDirective } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-landing',
   imports: [
     RouterLink,
     NzButtonComponent,
-    NgOptimizedImage
+    NzIconDirective
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
