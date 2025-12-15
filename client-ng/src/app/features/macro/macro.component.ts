93a12043e6f095dec342bb30cd68793b1479683c
Alex Cho <alex.jhcho@gmail.com>
2025-12-14 22:31:36 -0800

    feat(macro): get minimum functionality: margin and money market funds charts

--- client-ng/src/app/features/macro/macro.component.ts
+++ client-ng/src/app/features/macro/macro.component.ts
@@ -2,13 +2,20 @@ import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core
 import { MacroService } from '../../core/services/macro.service';
 import { SeriesObservations } from '../../shared/models/macro';
 import { NzCardComponent } from 'ng-zorro-antd/card';
-import { MacroChartComponent } from '../../shared/components/macro-chart/macro-chart.component';
+import { Chart } from 'chart.js/auto';
+import { MACRO_SERIES } from '../../shared/utils/macro-series';
+import { NzOptionComponent, NzSelectComponent } from 'ng-zorro-antd/select';
+import { KeyValuePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-macro',
   imports: [
     NzCardComponent,
-    MacroChartComponent
+    NzSelectComponent,
+    NzOptionComponent,
+    KeyValuePipe,
+    FormsModule,
   ],
   templateUrl: './macro.component.html',
   styleUrl: './macro.component.scss'
@@ -16,17 +23,66 @@ import { MacroChartComponent } from '../../shared/components/macro-chart/macro-c
 export class MacroComponent implements OnInit {
   private macroService = inject(MacroService);
 
-  protected seriesObservations: WritableSignal<SeriesObservations | undefined> = signal(undefined);
+  protected chart?: Chart;
+
+  protected seriesList: Record<string, string> = MACRO_SERIES;
+
   protected seriesId: WritableSignal<string> = signal('BOGZ1FL663067003Q');
+  protected seriesObservations: WritableSignal<SeriesObservations | undefined> = signal(undefined);
 
   ngOnInit() {
-    this.getSeriesObservations();
+    this.getSeriesObservationsAndCreateChart();
   }
 
-  private getSeriesObservations() {
+  protected getSeriesObservationsAndCreateChart() {
     this.macroService.getSeriesObservations(this.seriesId()).subscribe({
-      next: response => this.seriesObservations.set(response),
+      next: response => {
+        this.seriesObservations.set(response);
+        this.createMacroChart();
+      },
       error: err => console.log(err)
     })
+  }
+
+  private createMacroChart() {
+    const dataRead = this.seriesObservations()?.observations;
+
+    if (!dataRead) {
+      return;
+    }
+
+    this.chart?.destroy();
+
+    this.chart = new Chart('macroChart', {
+      type: 'line',
+      data: {
+        labels: dataRead.map(x => x.date),
+        datasets: [
+          {
+            label: 'Margin',
+            data: dataRead.map(x => x.value),
+            borderWidth: 1,
+            borderColor: 'rgba(0, 250, 0, 0.7)',
+            backgroundColor: 'rgba(0, 250, 0, 0.7)'
+          }
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {},
+          y: {
+            beginAtZero: true,
+          },
+        },
+        plugins: {
+          title: {
+            text: ``,
+            display: false
+          }
+        }
+      }
+    });
   }
 }
